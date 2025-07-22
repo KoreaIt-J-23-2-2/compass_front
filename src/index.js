dfda71eab3ff3197feeea11e63af0e4792b38321
wjdals717 <bjm3305@naver.com>
2025-07-22 18:02:33 +0900

    Chore: basename 추가

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <QueryClientProvider client={queryClient}>
     <RecoilRoot>
-      <BrowserRouter>
+      <BrowserRouter basename='/compass_front'>
       <ScrollToTop />
         <Global styles={Common} />
         <Reset />
