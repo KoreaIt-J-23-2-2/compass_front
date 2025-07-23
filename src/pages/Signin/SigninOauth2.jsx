58896f91ce52e63e899da316b65b98271861217d
wjdals717 <bjm3305@naver.com>
2025-07-23 20:28:32 +0900

    Fix: 404 오류 해결

--- src/pages/Signin/SigninOauth2.jsx
+++ src/pages/Signin/SigninOauth2.jsx
@@ -4,11 +4,13 @@ import { Navigate, useSearchParams } from 'react-router-dom';
 
 function SigninOauth2(props) {  // /auth/oauth2/signin
     // 서버로부터 redirection해서 들어온 token을 localStorage에 저장하는 과정
-    const [ searchParams, setSearchParams ] = useSearchParams();
+    const [ searchParams ] = useSearchParams();
     const queryClient = useQueryClient();
 
     localStorage.setItem("accessToken", "Bearer " + searchParams.get("token"));
     queryClient.refetchQueries(["getPrincipal"]);
+
+    console.log("accessToken: ", searchParams.get("token"));
 
     return <Navigate to={"/"} />
 }
